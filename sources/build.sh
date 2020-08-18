6b8fc6cfcd085337292c4dd2e90d56c48a055103
Eli Heuer <elih@protonmail.com>
2020-08-17 17:35:25 -0700

    Update build script for new GTL name

--- sources/build.sh
+++ sources/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fontmake -g sources/open-gate-naskh.glyphs -o ttf
+fontmake -g sources/GTL-Naskh.glyphs -o ttf
 rm -rf instance_ufo
 mkdir -p fonts/ttf
 cp -r master_ufo/*.designspace sources/
@@ -7,3 +7,5 @@ cp -r master_ufo/*.ufo sources/
 rm -rf master_ufo
 cp -r master_ttf/*.ttf fonts/ttf/
 rm -rf master_ttf
+
+python3 documentation/instagram/basic-specimen.py

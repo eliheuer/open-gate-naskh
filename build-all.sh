f33f06deeb5e787068cf7f0d8101636f62a6920b
Eli Heuer <elih@protonmail.com>
2020-11-01 08:18:20 -0800

    update main build script

--- build-all.sh
+++ build-all.sh
@@ -6,3 +6,4 @@ python3 documentation/print/booklet-test-recto.py
 python3 documentation/print/booklet-shams-verso.py
 python3 documentation/print/booklet-fatiha-recto.py
 python3 documentation/print/booklet-bab-verso.py
+python3 documentation/slide-decks/final-project-presentation/final-project-presentation.py
